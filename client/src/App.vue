d63931e3f6c879f36dec75356fe61370521f2a85
ChadH28 <chadhoosain@gmail.com>
2021-07-13 13:29:37 +0200

    worked on nav component

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,30 +1,28 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  <router-view/>
-  </div>
+    <div>
+        <Navbar/>
+        <router-view/>
+    </div>
 </template>
+
+<script>
+// @ is an alias to /src
+import Navbar from '@/components/Navbar.vue'
+export default {
+    components: {
+        Navbar
+    }
+}
+</script>
+
+
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
 }
 </style>
---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+        <nav class="navbar navbar-expand-lg navbar-light">
+            <div class="container-fluid">
+                <router-link class="navbar-brand" to="/"> Luma</router-link>
+            </div>
+        </nav>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+              </button>
+                <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                    <ul class="navbar-nav">
+                        <!-- <li class="nav-item">
+                            <router-link class="nav-link active" to="/">Home</router-link>
+                        </li> -->
+                        <li class="nav-item">
+                            <router-link class="nav-link" to="/about">What's new</router-link>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Woman</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Men</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Gear</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Training</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Sale</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </div>
+</template>
+
+
+
+
+    <router-link class="nav-link active" to="/">Home</router-link>
+    |
+    <router-link class="nav-link" to="/about">About</router-link>
+    |
+    <router-link class="nav-link" to="/login">Login</router-link>
