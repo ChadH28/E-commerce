9c55d58860548fac178993a381c8878a5e39be50
ChadH28 <chadhoosain@gmail.com>
2021-07-19 12:58:45 +0200

    worked on carousel and sidebar

--- client/src/views/Category.vue
+++ client/src/views/Category.vue
@@ -1,7 +1,7 @@
 <template>
-    <!-- sidebar -->
     <div>
         <div class="container">
+          <Sidebar/>
           <div class='control-bar'>
             <button class="btn btn-secondary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasScrolling" aria-controls="offcanvasScrolling">FILTER</button>
           </div>
@@ -36,103 +36,18 @@
               </div>
             </div>
           </div>
-        </div>
-        <div class="offcanvas offcanvas-start sidebar" data-bs-scroll="true" data-bs-backdrop="false" tabindex="-1" id="offcanvasScrolling" aria-labelledby="offcanvasScrollingLabel">
-            <div class="offcanvas-header">
-                <h3 class="offcanvas-title sidebar-title" id="offcanvasScrollingLabel">Tops</h3>
-                <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-            </div>
-            <div class="offcanvas-body">
-                <h4 class="sidebar-subtitle">Shopping Options</h4>
-                <hr>
-                <div class="accordion accordion-flush" id="accordion">
-                  <div class="accordion-item">
-                    <h2 class="accordion-header" id="flush-headingOne">
-                      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
-                        CATEGORY
-                      </button>
-                    </h2>
-                    <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordion">
-                      <div class="accordion-body">
-                          <a class="dropdown-item" href="#">Mens Tops</a>
-                          <a class="dropdown-item" href="#">Womans Tops</a>
-                          <a class="dropdown-item" href="#">Hoodies</a>
-                          <a class="dropdown-item" href="#">Pants</a>
-                          <a class="dropdown-item" href="#">Gear</a>
-                          <a class="dropdown-item" href="#">Training</a>
-                      </div>
-                    </div>
-                  </div>
-                  <div class="accordion-item">
-                    <h2 class="accordion-header" id="flush-headingTwo">
-                      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
-                        SIZE
-                      </button>
-                    </h2>
-                    <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordion">
-                      <div class="accordion-body">
-                        <a class='nav-item' href="#">XS</a> |
-                        <a class='nav-item' href="#">S</a> |
-                        <a class='nav-item' href="#">M</a> |
-                        <a class='nav-item' href="#">L</a> |
-                        <a class='nav-item' href="#">XL</a>
-                      </div>
-                    </div>
-                  </div>
-                  <div class="accordion-item">
-                    <h2 class="accordion-header" id="flush-headingThree">
-                      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
-                        STYLE
-                      </button>
-                    </h2>
-                    <div id="flush-collapseThree" class="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordion">
-                      <div class="accordion-body">Lorem ipsum dolor sit amet consectetur adipisicing elit. A amet pariatur voluptatum! Nesciunt, blanditiis cumque!</div>
-                    </div>
-                  </div>
-                  <div class="accordion-item">
-                    <h2 class="accordion-header" id="flush-headingFour">
-                      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseFour" aria-expanded="false" aria-controls="flush-collapseFour">
-                        CLIMATE
-                      </button>
-                    </h2>
-                    <div id="flush-collapseFour" class="accordion-collapse collapse" aria-labelledby="flush-headingFour" data-bs-parent="#accordion">
-                      <div class="accordion-body">
-                        <a class="dropdown-item" href="#">For the Hot Summer Days</a>
-                        <a class="dropdown-item" href="#">For the Cold Winter Days</a>
-                      </div>
-                    </div>
-                  </div>
-                  <div class="accordion-item">
-                    <h2 class="accordion-header" id="flush-headingSix">
-                      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseSix" aria-expanded="false" aria-controls="flush-collapseSix">
-                        MATERIAL
-                      </button>
-                    </h2>
-                    <div id="flush-collapseSix" class="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordion">
-                      <div class="accordion-body">Lorem ipsum dolor sit amet consectetur adipisicing elit. A amet pariatur voluptatum! Nesciunt, blanditiis cumque!</div>
-                    </div>
-                  </div>
-                  <div class="accordion-item">
-                    <h2 class="accordion-header" id="flush-headingSeven">
-                      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseSeven" aria-expanded="false" aria-controls="flush-collapseSeven">
-                        PRICE
-                      </button>
-                    </h2>
-                    <div id="flush-collapseSeven" class="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordion">
-                      <div class="accordion-body">Lorem ipsum dolor sit amet consectetur adipisicing elit. A amet pariatur voluptatum! Nesciunt, blanditiis cumque!</div>
-                    </div>
-                  </div>
-                </div>
-            </div>
-        </div>
+        </div> 
     </div>
 </template>
 
 <script>
-import {products} from '../data.js'
+import {products} from '../data.js';
+import Sidebar from '../components/Sidebar'
 export default {
     name: "Category",
-    components: {},
+    components: {
+      Sidebar
+    },
       data () {
         return {
           showProducts: true,
@@ -155,46 +70,7 @@ export default {
 </script>
 
 <style>
-.sidebar {
-    margin-top: 115px;
-    border-top: black 0.5px solid;
-}
 
-.sidebar-title {
-    padding: 15px 10px;
-    text-align: center;
-    font-size: 42px;
-}
-
-.sidebar-subtitle {
-    padding: 20px;
-}
-
-.nav ul {
- list-style-type: none;
-}
-
-.nav a {
-    background: none;
-    /* border-bottom: 1px solid #455a64; */
-    color: black;
-    text-transform: capitalize;
-    font-size: 14px;
-    padding: 16px 24px;
-}
-
-.active{
-  color: tomato
-}
-
-.nav a:hover {
-    background-color: #eceff1;
-    color: black
-}
-
-#nav-link-header{
-  border-top: 1px solid #455a64;
-}
 
 #card-img{
   height: 300px;
--- client/src/views/Product.vue
+++ client/src/views/Product.vue
@@ -2,33 +2,31 @@
   <div class="container">
     <div class='row'>
       <div class="col-6 col-md-6 col-sm-12 carousel-section">
-        <div class="carousel-container">
-          <div class="mySlides">
-            <img src="../assets/slim.jpg" style="width:100%">
+        <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+          <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
           </div>
-
-          <div class="mySlides">
-            <img src="../assets/slim.jpg" style="width:100%">
-          </div>
-
-          <div class="mySlides">
-            <img src="../assets/slim.jpg" style="width:100%">
-          </div>
-        
-          <a class="prev" onclick="plusSlides(-1)">❮</a>
-          <a class="next" onclick="plusSlides(1)">❯</a>
-
-          <div class="carousel-row">
-            <div class="carousel-column">
-              <img class="demo cursor" src="../assets/slim.jpg" style="width:100%" onclick="currentSlide(1)" alt="The Woods">
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img src="../assets/golfer.jpg" class="d-block w-100 img-fluid" alt="../assets/golfer.jpg">
             </div>
-            <div class="carousel-column">
-              <img class="demo cursor" src="../assets/slim.jpg" style="width:100%" onclick="currentSlide(2)" alt="Cinque Terre">
+            <div class="carousel-item">
+              <img src="../assets/golfer.jpg" class="d-block w-100 img-fluid" alt="../assets/golfer.jpg">
             </div>
-            <div class="carousel-column">
-              <img class="demo cursor" src="../assets/slim.jpg" style="width:100%" onclick="currentSlide(3)" alt="Mountains and fjords">
+            <div class="carousel-item">
+              <img src="../assets/golfer.jpg" class="d-block w-100 img-fluid" alt="../assets/golfer.jpg">
             </div>
           </div>
+          <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+          </button>
+          <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+          </button>
         </div>
       </div>
       
